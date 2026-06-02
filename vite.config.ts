845cbf5555c1437bac6378c8ccdf70566122b61a
udarasadaruwan <sadaruvan449@gmail.com>
2026-06-02 19:27:45 +0530

    change the project to compact to run with front end and backrnd

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,18 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/hela-eats-frontend/",
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://hela-eats-backend-api.onrender.com",
+        changeOrigin: true,
+        secure: true,
+      },
+      "/auth": {
+        target: "https://hela-eats-backend-api.onrender.com",
+        changeOrigin: true,
+        secure: true,
+      },
+    },
+  },
 });
